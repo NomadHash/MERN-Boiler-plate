4921a803610fa555da811017a8549c9b717f4706
Nomad_Hash <badsoap141@gmail.com>
2020-08-30 15:18:01 +0900

    Add create-react-app, react-router-dom

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" component={LandingPage} />
+          <Route path="/register" component={RegisterPage} />
+          <Route path="/login" component={LoginPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LandingPage = () => {
+  return <div>LandingPage 랜딩 페이지</div>;
+};
+
+export default LandingPage;
