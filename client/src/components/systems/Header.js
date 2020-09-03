4ff6006a79cb4f9d3086a98b0ce851a5ef907aed
Nomad_Hash <badsoap141@gmail.com>
2020-09-03 17:46:42 +0900

    Update mypage modal, README.md

--- client/src/components/systems/Header.js
+++ client/src/components/systems/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { withRouter } from 'react-router-dom';
 import logoImg from '../../public/rocket.png';
@@ -6,6 +6,10 @@ import axios from 'axios';
 let conditonRegistBtn = null;
 let conditonLoginBtn = null;
 const Header = (props) => {
+  //State-Hook
+  const [open, setOpen] = useState(false);
+
+  // React-Router
   const goLoginPage = () => {
     props.history.push('/login');
   };
@@ -26,12 +30,25 @@ const Header = (props) => {
     });
   };
 
+  const onMypageHandler = () => {
+    setOpen(!open);
+    console.log(open);
+  };
+
   props.isAuth === true
-    ? (conditonLoginBtn = <span>안녕하세요 {props.name} 님!</span>)
+    ? (conditonLoginBtn = (
+        <MypageBtn onClick={onMypageHandler}>
+          마이페이지
+          <ArcodianMypage open={open}>
+            <LogoutBtn onClick={onClickHandler}>내 정보</LogoutBtn>
+            <LogoutBtn onClick={onClickHandler}>로그아웃</LogoutBtn>
+          </ArcodianMypage>
+        </MypageBtn>
+      ))
     : (conditonLoginBtn = <LoginBtn onClick={goLoginPage}>Login</LoginBtn>);
 
   props.isAuth === true
-    ? (conditonRegistBtn = <button onClick={onClickHandler}>로그아웃</button>)
+    ? (conditonRegistBtn = '')
     : (conditonRegistBtn = (
         <SignUpBtn onClick={goRegistPage}>Sign up</SignUpBtn>
       ));
@@ -120,6 +137,39 @@ const SignUpBtn = styled.button`
   cursor: pointer;
   &:hover {
     box-shadow: 0px 4px 0px rgb(27 185 93);
+    outline: none;
+  }
+`;
+
+const ArcodianMypage = styled.div`
+  box-shadow: 0px 5px 0px rgba(0, 0, 0, 0.7);
+  position: fixed;
+  background: #eaeaea;
+  width: 136px;
+  height: 5vw;
+  border-radius: 15px;
+  align-items: flex-start;
+  display: ${(props) => (props.open === true ? 'flex' : 'none')};
+  flex-direction: column;
+  padding: 15px;
+`;
+const MypageBtn = styled.span`
+  color: #61dafb;
+  font-weight: 600;
+  font-size: 15px;
+  margin-right: 10px;
+`;
+
+const LogoutBtn = styled.button`
+  background: none;
+  border: none;
+  color: #474747;
+  cursor: pointer;
+  font-weight: 500;
+  font-size: 20px;
+  padding: 0px;
+  margin-bottom: 15px;
+  &:hover {
     outline: none;
   }
 `;
